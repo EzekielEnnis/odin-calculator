ff6466fff9609422b9de4cffcc3624130dae61a2
EzekielEnnis <143122905+EzekielEnnis@users.noreply.github.com>
2023-09-13 23:06:06 +0100

    Add rouch JS structure
    Add expected functions and elements needed with comments to keep track
    of what needs to be done.

---
+++ script.js
@@ -0,0 +1,38 @@
+const screenInput = document.querySelector(".calc-screen input")
+const screenCalc = document.querySelector(".calc-screen current-calc")
+const calc = document.querySelector(".calc-buttons")
+
+// Update calaculation
+const updateCalculation = () => {
+
+}
+
+// Calculate
+const operate = () => {
+    // do caculation
+    // constrict SF 
+}
+
+// Convert to decimal
+const decimal = () => {
+
+}
+
+// Check the operation
+const operatorPress = (e) => {
+    // check operator is dec or ac
+    // check if there is an operator active already
+        // replace
+    // check if there is an operator in calculation already 
+        // operate then add clicked operation
+}
+
+// On button press
+const btnPress = (e) => {
+    // if number
+
+    // if operator
+
+    // if equals
+}
+
