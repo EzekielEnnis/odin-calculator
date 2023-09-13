ee3bfa77c8e1931cbcb5b84cd96ef31a18131972
EzekielEnnis <143122905+EzekielEnnis@users.noreply.github.com>
2023-09-14 00:17:09 +0100

    Add basic functionality as a calculator
    Operate function now calculates and updateCalcualtion has its
    fundimental function

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
 const screenInput = document.querySelector(".calc-screen .input")
 const screenCalc = document.querySelector(".calc-screen .current-calc")
 
-// Update calaculation
-const updateCalculation = () => {
+/*
+TODO - cant click operation if theres no number input
+TODO - rework update Calculation so it works after one operation is completed
+TODO - finish decimal funtion 
+TODO - check if operation is already active and calcualte before appplying
+*/
 
+const opIds = {
+    "add": "+",
+    "sub": "-",
+    "mul": "x",
+    "div": "/"
+}
+
+// Stores operator before number is pressed
+let opStore = ""
+
+
+// Update calaculation
+const updateCalculation = (content) => {
+    screenCalc.textContent = `${screenInput.textContent} ${content}`
+    screenInput.textContent = ""
+    opStore = ""
 }
 
 // Calculate
 const operate = () => {
     // do caculation
+    let num1 = +screenCalc.textContent.split(" ")[0]
+    let op = screenCalc.textContent.split(" ")[1]
+    let num2 = +screenInput.textContent
+    let result
+
+    switch(op) {
+        case ("+"): result = num1 + num2; break;
+        case ("-"): result = num1 - num2; break;
+        case ("x"): result = num1 * num2; break;
+        case ("/"): result = num1 / num2; break;
+        default: "MATHERROR"
+    }
     // constrict SF 
+    if (result.length > 10) result.toPrecision(9)
+    screenCalc.textContent = result
+    screenInput.textContent = ""
 }
 
 // Convert to decimal
@@ -19,15 +54,22 @@ const decimal = () => {
 
 // Check the operation
 const operatorPress = (id) => {
+    const operators = ["=", "-", "x", "/"]
     // check operator is dec or ac
     if (id === "AC") {
         screenInput.textContent = ""
-        screenCalc.textContent = ""
+        screenCalc.textContent = "|"
+        return
     }
-    // check if there is an operator active already
-        // replace
+    else if (id === "dec") {
+        decimal()
+        return
+    }
+
     // check if there is an operator in calculation already 
-        // operate then add clicked operation
+    // operate then add clicked operation
+    opStore = id
+    console.log(opStore)
 }
 
 // On button press
@@ -38,12 +80,14 @@ const btnPress = (e) => {
     }
     // if number
     else if (e.className === "num"){
+        if (opStore != "") updateCalculation(opIds[opStore]);
         screenInput.textContent += e.id[3]
     }
     // if operator
     else if (e.className === "op") {
         operatorPress(e.id)
     }
+    else screenInput.textContent = "ERROR"
 }
 
 const btns = document.querySelectorAll("button")
